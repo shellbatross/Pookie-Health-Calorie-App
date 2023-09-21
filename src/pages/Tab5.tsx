60a8458c0584e59c66643a118720f9fb08042f99
Justin Tung <jtung@umd.edu>
2023-09-21 14:59:36 -0400

    tab5 done probably

--- src/pages/Tab5.tsx
+++ src/pages/Tab5.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import "./Tab5.scss"
 function Tab5(){
-    return (<div>brrrrrrrrrr brrrrrrrrrrrrrr brrrrrrrrrr</div>)
+    return (<div>
+        <h1 className='Title1'>Hi there</h1>
+        <h2 className='Title2'>Hello again</h2>
+    </div>
+    )
 
 }
 
