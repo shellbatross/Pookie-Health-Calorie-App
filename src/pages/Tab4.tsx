eda61c10906e171807eedd200c102255354398b0
Shraddha <shraddharamesh93@gmail.com>
2023-09-22 11:26:29 -0400

    blushie

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import "./Tab4.scss"
 
 function Tab4(){
-    return (<div>idkno im running out of what to type</div>)
+    const [state, setState] = useState(false);
+
+    const buttonClick = () => {
+    setState(true);
+    };
+
+    const closeNotification = () => {
+    setState(false);
+    };
+
+    // hidden button classname was made first to just make the button invisible
+    return (<div>
+                <button className='hidden-button' onClick={buttonClick}>
+                    <img src='https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/flushed-face.png' className='blushy-wushy' />
+                </button>
+                {state && (
+                    <div className="notification">
+                        <h1 className='notification-text'> Warning! Do not click on the profile image.</h1>
+                        <button onClick={closeNotification}>X</button>
+                   </div>
+                 )}
+                <h1 className='blushy-name'>Blushy J. Blush</h1>
+            </div>)
 
 }
 
