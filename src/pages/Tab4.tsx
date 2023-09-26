3e31f04a0d22844bb09887a1201b67cd0fb85b35
Shraddha <shraddharamesh93@gmail.com>
2023-09-25 21:22:44 -0400

    uahaha

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -3,14 +3,14 @@ import "./Tab4.scss"
 
 // source: https://www.w3schools.com/react/react_usestate.asp
 function Tab4(){
-    const [state, setState] = useState(false);
+    const [state, setState] = useState("off");
 
     function openNotification () {
-        setState(true);
+        setState("on");
     };
 
     function closeNotification () {
-        setState(false);
+        setState("off");
     };
 
     // hidden button classname was made first to just make the button invisible
@@ -20,7 +20,7 @@ function Tab4(){
                 <button className='hidden-button' onClick={openNotification}>
                     <img src='https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/flushed-face.png' className='blushy-wushy' />
                 </button>
-                {state != false && (
+                {state != "off" && (
                     <main className="notification">
                         <h1 className='warning-text'> Warning! Do not click on the profile image.</h1>
                         <button onClick={closeNotification}>X</button>
