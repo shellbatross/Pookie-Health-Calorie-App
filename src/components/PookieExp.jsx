06dad8eb0924ddba6fd773693abe5cc1d8afadf4
Shell <stephanie16066@gmail.com>
2023-10-01 12:21:50 -0400

    i forgot if i committed

--- src/components/PookieExp.jsx
+++ src/components/PookieExp.jsx
@@ -8,7 +8,7 @@ function PookieExp(){
       <ProgressBar>
       <ProgressBar striped variant="success" now={50} key={1} variant="currworkout" label = "x1.5 Experience!"/>
       <ProgressBar variant="warning" now={20} key={2} variant  ="dailygoal" label = "x1 Experience" />
-      <ProgressBar striped variant="danger" now={10} key={3} variant = "workoutgoal" label = "x2 Experience!"/>
+      <ProgressBar striped variant="danger" now={15} key={3} variant = "workoutgoal" label = "x2 Experience!"/>
     </ProgressBar>
     </div>
     )
