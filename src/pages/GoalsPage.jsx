79ea4b32d27ed0ecac1cb708780a9216bde9e89a
Bethel Biniam <bbiniam@terpmail.umd.edu>
2023-10-16 22:38:52 -0400

    functionally complete goals page, needs styling - bethel

--- src/pages/GoalsPage.jsx
+++ src/pages/GoalsPage.jsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import Plot from 'react-plotly.js';
+
 
 function GoalsPage(){
 
-    return (<div>
-        Meow
+    function progValues() {
+        var days = document.getElementsByName("days");
+        var currGoal = 5;
+        // TODO: stop hardcoding this value... grab it from the other file, when that person completes it
+        var goalsMet = [];
+
+        for(var i = 0; i < days.length; i++){
+            let incr = (parseInt(days[i].value) >= currGoal) ? 1 : 0;
+            if (i === 0) {
+                goalsMet[i] = incr;
+            } else {
+                goalsMet[i] = goalsMet[i - 1] + incr
+            }
+        }
+
+        return goalsMet
+    }
+
+    const [goals, setGoals] = useState({
+            x: [1, 2, 3, 4, 5, 6, 7],
+            y: progValues(),
+            type: 'line',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+            name: 'Goals Progress'
+        });
+
+    const [layout, setLayout] = useState({datarevision: 0});
+
+    const [revision, setRevision] = useState(0);
+
+    const updateChart = () => { 
+        goals.x = progValues();
+        setRevision(revision + 1 );
+        layout.datarevision = revision + 1;
+    };
+
+    return (<div id="page">
+        <h2 className="gptitle">Weekly Goal Progress</h2> <br />
+        <p>Current goal: 5 miles </p>
+
+        <p>Input 7 days worth of workout progress. </p> <br />
+
+        <form id="days-form" className="form-inline">
+        <label htmlFor="day1">Day 1:     </label>
+        <input type="number" id="day1" name="days" min="0" placeholder="0" />  <br />
+        
+        <label htmlFor="day2">Day 2:</label>
+        <input type="number" id="day2" name="days" min="0" placeholder="0" />  <br />
+
+        <label htmlFor="day3">Day 3:</label>
+        <input type="number" id="day3" name="days" min="0" placeholder="0" />  <br />
+
+        <label htmlFor="day4">Day 4:</label>
+        <input type="number" id="day4" name="days" min="0" placeholder="0" />  <br />
+
+        <label htmlFor="day5">Day 5:</label>
+        <input type="number" id="day5" name="days" min="0" placeholder="0" />  <br />
+        
+        <label htmlFor="day6">Day 6:</label>
+        <input type="number" id="day6" name="days" min="0" placeholder="0" />  <br />
+
+        <label htmlFor="day7">Day 7:</label>
+        <input type="number" id="day7" name="days" min="0" placeholder="0" />  
+        
+        <br /> <br />
+
+        <button type="button" className="btn btn-info" onClick={() => updateChart()}>Submit</button>
+        </form>
+
+        <Plot graphDiv="graph"
+            data={[
+            {
+                x: [1, 2, 3, 4, 5, 6, 7],
+                y: progValues(),
+                type: 'line',
+                marker: {color: 'blue'},
+            },
+            ]}
+
+            layout={ {width: 500, height: 400, title: 'Goals Met', tickmode: 'linear',
+                yaxis: {autorange: false, range: [0, 7], dtick: 1}, 
+                xaxis: {autorange: false, range: [1, 7], dtick: 1}} }
+        /> 
+
     </div>)
 
 }
